7ea822f2ab750d1f7276c6e9e59e75084070782d
Noah Drake <46735016+drakene@users.noreply.github.com>
2020-10-19 13:28:25 -0400

    Add files via upload

---
+++ TestAutomation/project/src/drivers/NelderMeadAlgorithm/NelderMeadTestDriver.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+
+import org.eclipse.stem.analysis.ErrorResult;
+import org.eclipse.stem.analysis.automaticexperiment.NelderMeadAlgorithm;
+
+public class NelderMeadTestDriver {
+
+	public static void main(String[] args) {
+		System.out.println("This is a test");
+	}
+
+}
