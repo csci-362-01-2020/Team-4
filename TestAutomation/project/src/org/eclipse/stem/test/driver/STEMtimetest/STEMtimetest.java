aec992fb06123ec503a44882de3559885d865f42
ryan <ryanoliverratliffe@gmail.com>
2020-11-17 01:09:03 -0500

    finished another test case, 20 completed test cases to date

--- TestAutomation/project/src/org/eclipse/stem/test/driver/STEMtimetest/STEMtimetest.java
+++ TestAutomation/project/src/org/eclipse/stem/test/driver/STEMtimetest/STEMtimetest.java
@@ -10,23 +10,17 @@ import org.eclipse.emf.ecore.EObject;
 
 public class STEMtimetest{
 
-	// One day's worth of milliseconds
-	protected long TEST_INCREMENT;
-
-	/**
-	 * <!-- begin-user-doc --> <!-- end-user-doc -->
-	 * @generated
-	 */
 	public static void main(String[] args) {
-		
+		//parses command line args into usable variables
 		int TEST_INCREMENT = Integer.parseInt(args[0]);
 		boolean oracle = Boolean.parseBoolean(args[1]);
 		
+		//creates a new instance of stemtime to test and tests it
 		STEMTime startTime = ModelFactory.eINSTANCE.createSTEMTime();
 		STEMTime newTime = startTime.addIncrement(TEST_INCREMENT);
 		boolean TestIncrement = newTime.getTime().getTime() - startTime.getTime().getTime() == TEST_INCREMENT;
 
-		
+		//sends results off to the report
 		TestReporter tr = new TestReporter();
 		tr.validate(TestIncrement);
 		tr.reportTest(new ArrayList<String>(Arrays.asList(Boolean.toString(TestIncrement))));
